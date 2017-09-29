32dc2ef400fb9f7bab34797bc480f04b4f9e46b2
Punpun <stefan.neotokyo@gmail.com>
2017-09-29 16:41:14 +0200

    Initial commit

---
+++ PinApp/Controller/MainVC.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  PinApp
+//
+//  Created by Stefan Markovic on 9/29/17.
+//  Copyright © 2017 Stefan Markovic. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Its cool man")
+
+    }
+
+
+
+
+}
+
